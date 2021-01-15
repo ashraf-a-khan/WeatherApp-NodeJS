b7e0454634721b4177dddfae41549ea40f7b700f
ashraf-a-khan <khan.ashraf2311@gmail.com>
2021-01-15 18:02:47 -0500

    Adds forecast and geocode in utils to be exported

--- app.js
+++ app.js
@@ -21,20 +21,10 @@ const forecast = require("./utils/forecast");
 //   }
 // });
 
-// geocode("Boston", (error, data) => {
-//   console.log("Error", error);
-//   console.log("Data", data);
-// });
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+geocode("Boston", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
+});
 
 forecast(37.765, -122.241, (error, data) => {
   console.log("Error", error);
